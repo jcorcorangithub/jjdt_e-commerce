5a2af39b2f58bd3a49231714ca22f56dd8ccbbbf
Daisha Marrow <marrowdaisha@gmail.com>
2021-07-22 21:20:56 -0400

    created homepage and product page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from './screens/HomeScreen'
+import ProductPage from "./screens/ProductPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload this is a test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+            <Route path='/' component={HomeScreen} exact/>
+            <Route path='/product/:id' component={ProductPage}/>
+
+        </Container>
+  
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
