568ccbce0aca4f2ed27eb49331ed30ce9ee477f4
James Corcoran <jpcsoccer1234@gmail.com>
2021-07-27 20:47:16 -0400

    changed public to build on server.js

--- server/server.js
+++ server/server.js
@@ -26,11 +26,11 @@ app.use(express.json());
 app.use(fileUpload());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(`${__dirname}, '../client/public'`)));
+  app.use(express.static(path.join(`${__dirname}, '../client/build'`)));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.post("/uploads", (req, res) => {
